a46334b41158a7dcf67df29c21675b0ec0b6d1fb
Rodolfo Roman <rromanv@gmail.com>
2022-09-22 20:31:06 -0500

    using secrets

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const title = ref(import.meta.env.VITE_APP_NAME)
+const message = ref(import.meta.env.VITE_MESSAGE)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main
+    class="flex flex-col justify-center items-center min-h-screen bg-gray-900 gap-16"
+  >
+    <h1 class="text-8xl font-light text-gray-400">{{ title }}!</h1>
+    <p class="text-3xl font-serif text-yellow-600">{{ message }}!</p>
+  </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
